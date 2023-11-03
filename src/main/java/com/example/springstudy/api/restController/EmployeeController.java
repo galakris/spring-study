bdf8f53e98b04c3436761adc3efa19153e64b434
galakris <krzysztof.galuszka95@gmail.com>
2023-11-03 09:44:03 +0100

    Add Spring HATEOAS

--- src/main/java/com/example/springstudy/api/restController/EmployeeController.java
+++ src/main/java/com/example/springstudy/api/restController/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.example.springstudy.api.restController;
 
+import com.example.springstudy.api.EmployeeModelAssembler;
 import com.example.springstudy.api.EmployeeNotFoundException;
 import com.example.springstudy.api.model.Employee;
-import com.example.springstudy.api.repository.CarRepository;
 import com.example.springstudy.api.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,33 +15,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
 
     EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
+    EmployeeModelAssembler assembler;
+
+    public EmployeeController(EmployeeRepository employeeRepository, EmployeeModelAssembler employeeModelAssembler) {
         this.employeeRepository = employeeRepository;
+        this.assembler = employeeModelAssembler;
     }
 
     @GetMapping("/api/employees")
-    List<Employee> getAll() {
-        return employeeRepository.findAll();
-    }
-
-    @PostMapping("/api/employees")
-    Employee saveEmployee(@RequestBody Employee employee) {
-        return employeeRepository.save(employee);
+    public CollectionModel<EntityModel<Employee>> getAll() {
+        List<EntityModel<Employee>> employees = employeeRepository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).getAll()).withSelfRel());
     }
 
     @GetMapping("/api/employees/{id}")
-    Employee getEmployeeById(@PathVariable Long id) {
-        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    public EntityModel<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toModel(employee);
     }
 
+
+    @PostMapping("/api/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
     @PutMapping("/api/employees/{id}")
-    Employee putEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee){
+    public Employee putEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee){
         // should be moved to service
         return employeeRepository.findById(id).
                 map(employee -> {
@@ -58,7 +69,7 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/api/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    public void deleteEmployee(@PathVariable Long id) {
         employeeRepository.deleteById(id);
     }
 
